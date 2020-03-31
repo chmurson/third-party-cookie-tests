20c6a5015eaff1754362821576538bc096c6dc18
Wojciech Piątkowski <chmurson@gmail.com>
2020-03-31 20:49:55 +0200

    Updates

---
+++ precache-manifest.5e14bf343d857d2b887c633c46aeaa57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20902c5a41ecb8440f76ce809cb68a5",
+    "url": "/third-party-cookie-tests/index.html"
+  },
+  {
+    "revision": "c25381993d475498ec1a",
+    "url": "/third-party-cookie-tests/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "655ed8aa8c5e90b8f41d",
+    "url": "/third-party-cookie-tests/static/js/2.71a016d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/third-party-cookie-tests/static/js/2.71a016d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25381993d475498ec1a",
+    "url": "/third-party-cookie-tests/static/js/main.f3d4d2dc.chunk.js"
+  },
+  {
+    "revision": "d73d961013e67bab271c",
+    "url": "/third-party-cookie-tests/static/js/runtime-main.297596a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/third-party-cookie-tests/static/media/logo.5d5d9eef.svg"
+  }
+]);
