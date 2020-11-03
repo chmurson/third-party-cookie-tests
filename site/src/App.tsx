4f29136bb71b62cc725f10bd9b38308216bbb24a
Wojciech Piątkowski <chmurson@gmail.com>
2020-11-03 22:17:51 +0100

    bootstrap site; update README of third-party content; provide netlify.toml to site and third party content

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
-
-import { sitesUrls } from 'common'
-
-import './App.css'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          3rd Party Cookies Test Site A
-        </a>
-        <a href={sitesUrls.siteB} target="_blank">
-          Go to Site B
+          Learn React
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
