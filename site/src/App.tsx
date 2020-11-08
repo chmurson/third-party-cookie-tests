adfada3d26b3a0b3ee1d5e4d2746eec01fa2f33b
Wojciech Piątkowski <chmurson@gmail.com>
2020-11-08 23:29:18 +0100

    to switch from primereact to antd for the main site code

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,31 +1,37 @@
 import React, { useCallback, useState } from 'react'
 import styles from './App.module.scss'
-import { InputSwitch } from 'primereact/inputswitch'
+import { Radio } from 'antd'
 import clsx from 'clsx'
 
 const SHOWCOOKIE2ME_URL = process.env.REACT_APP_SHOWCOOKIE2ME_URL
 console.log(process.env)
 
+type HiddenIframeOptions = 'Visible' | 'Hidden';
+const hiddenIframeOptions: HiddenIframeOptions[] = ['Visible', 'Hidden']
+
 function App() {
-    const [iframeVisible, setIframeVisible] = useState(false)
+    const [iframeVisible, setIframeVisible] = useState<HiddenIframeOptions>('Hidden')
     const handleIframeSwitchChange = useCallback(e => {
-        setIframeVisible(e.value)
+        setIframeVisible(e.target.value)
     }, [])
 
-    console.log('render')
+
     return (
         <div className={styles.App}>
             <div>
                 <h2>A site {window.location.host}</h2>
 
                 <div className="p-field p-grid">
-                    <label htmlFor="firstname1" className="p-col-fixed">Visible iframe</label>
+                    <label htmlFor="firstname1" className="p-col-fixed">Iframe</label>
                     <div className="p-col p-justify-start">
-                        <InputSwitch checked={iframeVisible} onChange={handleIframeSwitchChange}
-                                     ariaLabelledBy="iframe-switch" />
+                        <Radio.Group options={hiddenIframeOptions}
+                                     value={iframeVisible}
+                                     onChange={handleIframeSwitchChange}
+                                     optionType="button"
+                                     buttonStyle="outline" />
                     </div>
                 </div>
-                <p className={clsx({ [styles.invisible]: !iframeVisible })}>Iframe URL: {SHOWCOOKIE2ME_URL}</p>
+                <p className={clsx({ [styles.invisible]: iframeVisible === 'Hidden' })}>Iframe URL: {SHOWCOOKIE2ME_URL}</p>
             </div>
             <iframe title="A note maker" className={clsx(styles.iframe, { [styles.iframeVisible]: iframeVisible })}
                     src={SHOWCOOKIE2ME_URL} />
